5b9859737030177d3f5f95ea32c9961c89267329
JideOgun <JideOgun@github.com>
2021-12-29 00:14:12 -0600

    end of module 9

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
@@ -136,17 +136,25 @@ if(!portfolioData.projects) {
 
 };
 
-promptUser().then(promptProject)
+promptUser()
+.then(promptProject)
 .then(portfolioData => {
-   const pageHTML = generatePage (portfolioData);
-
-fs.writeFile('index.html', pageHTML, err => {
-if (err) throw err;
-
-console.log('Portfolio created! Checkout index.html in this directory to see it!');
-}
-);
+   return generatePage (portfolioData);
+})
+.then(pageHTML => {
+  return writeFile(pageHTML);
+})
+.then(writeFileResponse => {
+  console.log(writeFileResponse);
+  return fs.copyFile();
+})
+.then (copyFileResponse => {
+  console.log(copyFileResponse);
+})
+.catch(err => {
+  console.log(err);
 });
+
 
 
 
